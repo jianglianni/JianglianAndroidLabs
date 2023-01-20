92e4ae18c12a6bd8bd86c692b143c520ae4b5430
jianglian <jianglian909.ni@gmail.com>
2023-01-19 23:42:27 -0500

    first commit

---
+++ app/src/main/java/algonquin/cst2335/ni000017/MainActivity.java
@@ -0,0 +1,13 @@
+package algonquin.cst2335.ni000017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
