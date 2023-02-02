812b36ea172ed586988f07263134ecdadd35f332
jianglian <jianglian909.ni@gmail.com>
2023-02-02 12:58:28 -0500

    FiFinished Phone Version of Lab 3

--- app/src/main/java/algonquin/cst2335/ni000017/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/ni000017/MainActivity.java
@@ -1,13 +1,41 @@
 package algonquin.cst2335.ni000017;
 
+import static algonquin.cst2335.ni000017.R.id.imageView;
+import static algonquin.cst2335.ni000017.R.id.spin_switch;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imgView;
+    Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgView = findViewById(R.id.imageView);
+        sw = findViewById(R.id.spin_switch);
+        sw.setOnCheckedChangeListener((btn,isChecked) -> {
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF,
+                        0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+                imgView.startAnimation(rotate);
+            }
+            else {
+                imgView.clearAnimation();
+            }
+        });
+
     }
 }
