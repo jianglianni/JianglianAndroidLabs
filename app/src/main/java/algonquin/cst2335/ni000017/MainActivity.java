af58970ad0af74f79c7c7d98c750ed333e0e0a02
jianglian <jianglian909.ni@gmail.com>
2023-02-01 23:24:29 -0500

    Finished step 2

--- app/src/main/java/algonquin/cst2335/ni000017/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/ni000017/MainActivity.java
@@ -1,6 +1,6 @@
 package algonquin.cst2335.ni000017;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
